04de62a1e296655bd0a32be200e0c516d0cbdfd4
Mosaab <76660323+MosaabRkia@users.noreply.github.com>
2021-03-25 16:49:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxPrice = new System.Windows.Forms.TextBox();
+            this.txtBoxSrcImg = new System.Windows.Forms.TextBox();
+            this.txtBoxDescription = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxID = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SelectList = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxName
+            // 
+            resources.ApplyResources(this.txtBoxName, "txtBoxName");
+            this.txtBoxName.Name = "txtBoxName";
+            // 
+            // txtBoxPrice
+            // 
+            resources.ApplyResources(this.txtBoxPrice, "txtBoxPrice");
+            this.txtBoxPrice.Name = "txtBoxPrice";
+            // 
+            // txtBoxSrcImg
+            // 
+            resources.ApplyResources(this.txtBoxSrcImg, "txtBoxSrcImg");
+            this.txtBoxSrcImg.Name = "txtBoxSrcImg";
+            // 
+            // txtBoxDescription
+            // 
+            resources.ApplyResources(this.txtBoxDescription, "txtBoxDescription");
+            this.txtBoxDescription.Name = "txtBoxDescription";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txtBoxID
+            // 
+            resources.ApplyResources(this.txtBoxID, "txtBoxID");
+            this.txtBoxID.Name = "txtBoxID";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SelectList
+            // 
+            resources.ApplyResources(this.SelectList, "SelectList");
+            this.SelectList.Name = "SelectList";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.SelectList);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxDescription);
+            this.Controls.Add(this.txtBoxSrcImg);
+            this.Controls.Add(this.txtBoxPrice);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.txtBoxID);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxPrice;
+        private System.Windows.Forms.TextBox txtBoxSrcImg;
+        private System.Windows.Forms.TextBox txtBoxDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxID;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label SelectList;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        string pathit = @"../../file1.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void DoSomething(string str)
+        {
+            string conStr = @"Data Source=DESKTOP-JG3F6E8\SQLEXPRESS;Initial Catalog=ShopZoid;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+
+            SqlCommand comm = new SqlCommand(str,con);
+            con.Open();
+            int Results = comm.ExecuteNonQuery();
+            con.Close();
+            if (Results == 1)
+            {
+                MessageBox.Show("Sucess!");
+            }
+            else
+            {
+                MessageBox.Show(" not Sucess!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText(pathit, txtBoxID.Text + "','" + txtBoxName.Text + "','" + txtBoxSrcImg.Text + "','" + txtBoxDescription.Text + "','" + txtBoxPrice.Text);
+           // DoSomething("INSERT INTO itemsList(id,name,srcImg,description,price) VALUES('" + txtBoxID.Text + "','" + txtBoxName.Text + "','" + txtBoxSrcImg.Text + "','" + txtBoxDescription.Text + "','" + txtBoxPrice.Text + "') ");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(pathit, "");
+           // DoSomething($"DELETE itemsList WHERE id={txtBoxID.Text}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           // DoSomething($"UPDATE itemsList SET  price={txtBoxPrice.Text}  WHERE id={txtBoxID.Text}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*
+            string example = "";
+            string conStr = @"Data Source=DESKTOP-JG3F6E8\SQLEXPRESS;Initial Catalog=ShopZoid;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+
+            SqlCommand comm = new SqlCommand("SELECT * FROM itemsList", con);
+
+            con.Open();
+            SqlDataReader Reader = comm.ExecuteReader();
+            while (Reader.Read())
+            {
+                example += "ID:" + Reader["id"] + " , Name:" + Reader["name"] + " , Desc:" + Reader["description"] + " , Price:" + Reader["price"] + "\n";
+            }
+            con.Close();*/
+           string[] example = File.ReadAllLines(pathit);
+            SelectList.Text = "";
+            int counter = 1;
+            foreach (string line in example){
+                SelectList.Text += "\n" + counter + " :" +  line ;
+                counter++;
+            }
+
+          //  SelectList.Text = example;
+        }
+    }
+}
+
+/*
+if (/*txtBoxID.Text != "" && txtBoxName.Text != "" && txtBoxPrice.Text != "" && txtBoxSrcImg.Text != "" && txtBoxDescription.Text != "" /*&& txtBoxID.Text != null && txtBoxName.Text != null && txtBoxPrice.Text != null && txtBoxSrcImg.Text != null && txtBoxDescription.Text != null)
+{
+}
+*/
